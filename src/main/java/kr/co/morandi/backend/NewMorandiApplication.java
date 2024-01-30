4d83999b80b30c31326ae34aa8eb6f7fc352ea96
miiiinju1 <gms07073@ynu.ac.kr>
2024-01-30 14:59:23 +0900

    :tada: Initial Commit

---
+++ src/main/java/kr/co/morandi/backend/NewMorandiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.morandi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMorandiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewMorandiApplication.class, args);
+    }
+
+}
