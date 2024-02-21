93c2b2bc287a5c3e1f817aee3197e78bce8b78ef
miiiinju1 <gms07073@ynu.ac.kr>
2024-02-21 17:40:47 +0900

    :fire: 사용하지 않는 import 제거

--- src/main/java/kr/co/morandi/backend/domain/member/MemberRepository.java
+++ src/main/java/kr/co/morandi/backend/domain/member/MemberRepository.java
@@ -1,7 +1,5 @@
 package kr.co.morandi.backend.domain.member;
 
-
-import kr.co.morandi.backend.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
