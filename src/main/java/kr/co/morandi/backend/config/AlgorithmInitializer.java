985ad63abf752a5866aa02d787cb850b247373ef
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-02-27 15:38:31 +0900

    Algorithm 테이블 초기화 기능 추가 (#21)
    * :art: Algorithm 필드 추가
    
    Algorithm 테이블에 solved에서 취급하는 key와 bojTagId를 추가했습니다.
    
    * :sparkles: 어플리케이션 시작 시 알고리즘 테이블 초기화
    
    * :white_check_mark: AlgorithmRepository Test tearDown 메서드 추가
    
    * :bug: @ Value Algorithms.json 주소 변경

---
+++ src/main/java/kr/co/morandi/backend/config/AlgorithmInitializer.java
@@ -0,0 +1,51 @@
+package kr.co.morandi.backend.config;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.annotation.Value;
+
+import kr.co.morandi.backend.domain.algorithm.Algorithm;
+import kr.co.morandi.backend.domain.algorithm.AlgorithmRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class AlgorithmInitializer {
+
+    private final AlgorithmRepository algorithmRepository;
+    private final ObjectMapper objectMapper;
+
+    @Value("classpath:Algorithms.json")
+    private Resource algorithmsResource;
+
+    @PostConstruct
+    @Transactional
+    public void init() throws IOException {
+        final List<Algorithm> initialAlgorithms = objectMapper.readValue(algorithmsResource.getInputStream(), new TypeReference<>() {});
+        final List<Algorithm> uninitialized = collectUninitialized(initialAlgorithms);
+
+        algorithmRepository.saveAll(uninitialized);
+    }
+
+    private List<Algorithm> collectUninitialized(List<Algorithm> initialAlgorithms) {
+        final List<Algorithm> findAll = algorithmRepository.findAll();
+
+        final Set<Integer> collect = findAll.stream()
+                .map(Algorithm::getBojTagId)
+                .collect(Collectors.toSet());
+
+        return initialAlgorithms.stream()
+                .filter(algorithm -> !collect.contains(algorithm.getBojTagId()))
+                .toList();
+    }
+
+}
