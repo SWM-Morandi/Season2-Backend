6fb6aaa8cbc288cc1997a36ca62c4cd87a8eb8d7
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2024-02-27 15:38:31 +0900

    Algorithm 테이블 초기화 기능 추가 (#21)
    * :art: Algorithm 필드 추가
    
    Algorithm 테이블에 solved에서 취급하는 key와 bojTagId를 추가했습니다.
    
    * :sparkles: 어플리케이션 시작 시 알고리즘 테이블 초기화
    
    * :white_check_mark: AlgorithmRepository Test tearDown 메서드 추가
    
    * :bug: @ Value Algorithms.json 주소 변경

--- src/main/java/kr/co/morandi/backend/domain/algorithm/AlgorithmRepository.java
+++ src/main/java/kr/co/morandi/backend/domain/algorithm/AlgorithmRepository.java
@@ -2,5 +2,8 @@ package kr.co.morandi.backend.domain.algorithm;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AlgorithmRepository extends JpaRepository<Algorithm, Long> {
+    Boolean existsByBojTagIdOrAlgorithmKey(Integer bojTagId, String algorithmKey);
+
 }
---
+++ src/test/java/kr/co/morandi/backend/domain/algorithm/AlgorithmRepositoryTest.java
@@ -0,0 +1,53 @@
+package kr.co.morandi.backend.domain.algorithm;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AlgorithmRepositoryTest {
+
+    @Autowired
+    private AlgorithmRepository algorithmRepository;
+    @AfterEach
+    void tearDown() {
+        algorithmRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("알고리즘 초기 데이터가 존재하는지 확인한다.")
+    @Test
+    void existsByBojTagIdOrAlgorithmKey() {
+        // given
+        Algorithm algorithm1 = Algorithm.builder()
+                .bojTagId(1)
+                .algorithmKey("key1")
+                .algorithmName("name1")
+                .build();
+
+        Algorithm algorithm2 = Algorithm.builder()
+                .bojTagId(2)
+                .algorithmKey("key2")
+                .algorithmName("name2")
+                .build();
+
+        algorithmRepository.saveAll(List.of(algorithm1, algorithm2));
+
+        // when
+        boolean exists1 = algorithmRepository.existsByBojTagIdOrAlgorithmKey(1, "key1");
+        boolean exists2 = algorithmRepository.existsByBojTagIdOrAlgorithmKey(2, "key2");
+
+
+        // then
+        assertThat(exists1).isTrue();
+        assertThat(exists2).isTrue();
+    }
+
+}
