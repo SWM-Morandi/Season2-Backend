0745eef478fec5b87d9fcb4e923faf1bd36e5e3c
miiiinju1 <gms07073@ynu.ac.kr>
2024-03-02 18:57:13 +0900

    :white_check_mark: DailyTestRecordTest 수정

--- src/test/java/kr/co/morandi/backend/domain/contentrecord/dailytest/DailyTestRecordTest.java
+++ src/test/java/kr/co/morandi/backend/domain/contentrecord/dailytest/DailyTestRecordTest.java
@@ -2,16 +2,11 @@ package kr.co.morandi.backend.domain.contentrecord.dailytest;
 
 import kr.co.morandi.backend.domain.contentproblemrecord.ContentProblemRecord;
 import kr.co.morandi.backend.domain.contenttype.dailytest.DailyTest;
+import kr.co.morandi.backend.domain.contenttype.dailytest.DailyTestProblems;
 import kr.co.morandi.backend.domain.member.Member;
-import kr.co.morandi.backend.domain.member.MemberRepository;
 import kr.co.morandi.backend.domain.problem.Problem;
-import kr.co.morandi.backend.domain.problem.ProblemRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
@@ -21,31 +16,19 @@ import static kr.co.morandi.backend.domain.contenttype.tier.ProblemTier.*;
 import static kr.co.morandi.backend.domain.member.SocialType.GOOGLE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.groups.Tuple.tuple;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
 @ActiveProfiles("test")
 class DailyTestRecordTest {
-
-    @Autowired
-    private ProblemRepository problemRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
-    @AfterEach
-    void tearDown() {
-        memberRepository.deleteAllInBatch();
-        problemRepository.deleteAllInBatch();
-    }
     @DisplayName("오늘의 문제 기록이 만들어졌을 때 푼 문제 수는 0문제 이어야 한다.")
     @Test
     void solvedCountIsZero() {
         // given
-        List<Problem> problems = createProblems();
-        LocalDateTime createdTime = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
+        DailyTest dailyTest = createDailyTest();
         LocalDateTime startTime = LocalDateTime.of(2024, 3, 1, 12, 0, 0);
-        DailyTest dailyTest = DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
         Member member = createMember("user");
+        List<Problem> problems = getProblemList(dailyTest);
 
         // when
         DailyTestRecord dailyTestRecord = DailyTestRecord.create(dailyTest.getProblemCount(), startTime, dailyTest, member, problems);
@@ -53,30 +36,38 @@ class DailyTestRecordTest {
         // then
         assertThat(dailyTestRecord.getSolvedCount()).isZero();
     }
-    @DisplayName("오늘의 문제 기록이 만들어졌을 때 전체 문제 수는 오늘의 문제에 출제된 문제 수와 같아야 한다.")
+    @DisplayName("오늘의 문제 기록이 만들어졌을 때 전체 문제 수는 오늘의 문제에 출제된 문제 들과 같아야 한다.")
     @Test
     void problemCountIsEqual() {
         // given
-        List<Problem> problems = createProblems();
-        LocalDateTime createdTime = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
+        DailyTest dailyTest = createDailyTest();
         LocalDateTime startTime = LocalDateTime.of(2024, 3, 1, 12, 0, 0);
-        DailyTest dailyTest = DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
         Member member = createMember("user");
+        List<Problem> problems = getProblemList(dailyTest);
 
         // when
         DailyTestRecord dailyTestRecord = DailyTestRecord.create(dailyTest.getProblemCount(), startTime, dailyTest, member, problems);
 
         // then
         assertThat(dailyTestRecord.getProblemCount()).isEqualTo(dailyTest.getProblemCount());
+        assertThat(dailyTestRecord.getContentProblemRecords())
+                .extracting("problem", "contentRecord")
+                .containsExactlyInAnyOrder(
+                        tuple(problems.get(0), dailyTestRecord),
+                        tuple(problems.get(1), dailyTestRecord),
+                        tuple(problems.get(2), dailyTestRecord)
+                );
     }
     @DisplayName("오늘의 문제 기록이 만들어진 시점이 문제가 출제된 시점에서 하루 이상 넘어가면 예외가 발생한다.")
     @Test
-    void recordCreatedWithinOneDay() {
+    void recordCreateExceptionWhenOverOneDay() {
         // given
-        List<Problem> problems = createProblems();
         LocalDateTime createdTime = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
-        DailyTest dailyTest = DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
+
+        DailyTest dailyTest = createDailyTest(createdTime);
         Member member = createMember("user");
+        List<Problem> problems = getProblemList(dailyTest);
+
         LocalDateTime startTime = LocalDateTime.of(2024, 3, 2, 0, 0, 0);
         // when & then
         assertThatThrownBy(() -> DailyTestRecord.create(dailyTest.getProblemCount(), startTime, dailyTest, member, problems))
@@ -85,12 +76,14 @@ class DailyTestRecordTest {
     }
     @DisplayName("오늘의 문제 기록이 만들어진 시점이 문제가 출제된 시점에서 하루 이상 넘어가지 않으면 정상적으로 등록된다.")
     @Test
-    void recordCreatedWithinOneDay2() {
+    void recordCreatedWithinOneDay() {
         // given
-        List<Problem> problems = createProblems();
         LocalDateTime createdTime = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
-        DailyTest dailyTest = DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
+
+        DailyTest dailyTest = createDailyTest(createdTime);
         Member member = createMember("user");
+        List<Problem> problems = getProblemList(dailyTest);
+
         LocalDateTime startTime = LocalDateTime.of(2024, 3, 1, 23, 59, 59);
 
         // when
@@ -103,11 +96,10 @@ class DailyTestRecordTest {
     @Test
     void isSolvedIsFalse() {
         // given
-        List<Problem> problems = createProblems();
-        LocalDateTime createdTime = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
+        DailyTest dailyTest = createDailyTest();
         LocalDateTime startTime = LocalDateTime.of(2024, 3, 1, 12, 0, 0);
-        DailyTest dailyTest = DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
         Member member = createMember("user");
+        List<Problem> problems = getProblemList(dailyTest);
 
         // when
         DailyTestRecord dailyTestRecord = DailyTestRecord.create(dailyTest.getProblemCount(), startTime, dailyTest, member, problems);
@@ -122,11 +114,10 @@ class DailyTestRecordTest {
     @Test
     void submitCountIsZero() {
         // given
-        List<Problem> problems = createProblems();
-        LocalDateTime createdTime = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
+        DailyTest dailyTest = createDailyTest();
         LocalDateTime startTime = LocalDateTime.of(2024, 3, 1, 12, 0, 0);
-        DailyTest dailyTest = DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
         Member member = createMember("user");
+        List<Problem> problems = getProblemList(dailyTest);
 
         // when
         DailyTestRecord dailyTestRecord = DailyTestRecord.create(dailyTest.getProblemCount(), startTime, dailyTest, member, problems);
@@ -141,11 +132,10 @@ class DailyTestRecordTest {
     @Test
     void solvedCodeIsNull() {
         // given
-        List<Problem> problems = createProblems();
-        LocalDateTime createdTime = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
+        DailyTest dailyTest = createDailyTest();
         LocalDateTime startTime = LocalDateTime.of(2024, 3, 1, 12, 0, 0);
-        DailyTest dailyTest = DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
         Member member = createMember("user");
+        List<Problem> problems = getProblemList(dailyTest);
 
         // when
         DailyTestRecord dailyTestRecord = DailyTestRecord.create(dailyTest.getProblemCount(), startTime , dailyTest, member, problems);
@@ -156,15 +146,27 @@ class DailyTestRecordTest {
                 .extracting("solvedCode")
                 .containsExactlyInAnyOrder(null, null, null);
     }
-
+    private List<Problem> getProblemList(DailyTest dailyTest) {
+        return dailyTest.getDailyTestProblemsList().stream()
+                .map(DailyTestProblems::getProblem)
+                .toList();
+    }
+    private DailyTest createDailyTest() {
+        List<Problem> problems = createProblems();
+        LocalDateTime createdTime = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
+        return DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
+    }
+    private DailyTest createDailyTest(LocalDateTime createdTime) {
+        List<Problem> problems = createProblems();
+        return DailyTest.create(createdTime, "오늘의 문제 테스트", problems);
+    }
     private List<Problem> createProblems() {
         Problem problem1 = Problem.create(1L, B5, 0L);
         Problem problem2 = Problem.create(2L, S5, 0L);
         Problem problem3 = Problem.create(3L, G5, 0L);
-        return problemRepository.saveAll(List.of(problem1, problem2, problem3));
+        return List.of(problem1, problem2, problem3);
     }
     private Member createMember(String name) {
-        Member member = Member.create(name, name + "@gmail.com", GOOGLE, name, name);
-        return memberRepository.save(member);
+        return Member.create(name, name + "@gmail.com", GOOGLE, name, name);
     }
 }
