91d731ba396ab2905e076638a8218e6544c4c566
miiiinju1 <gms07073@ynu.ac.kr>
2024-03-03 14:35:16 +0900

    :white_check_mark: CustomDefenseRecordTest when절 수정

--- src/test/java/kr/co/morandi/backend/domain/contentrecord/customdefense/CustomDefenseRecordTest.java
+++ src/test/java/kr/co/morandi/backend/domain/contentrecord/customdefense/CustomDefenseRecordTest.java
@@ -1,6 +1,5 @@
 package kr.co.morandi.backend.domain.contentrecord.customdefense;
 
-import kr.co.morandi.backend.domain.contentproblemrecord.ContentProblemRecord;
 import kr.co.morandi.backend.domain.contenttype.customdefense.CustomDefense;
 import kr.co.morandi.backend.domain.contenttype.customdefense.CustomDefenseProblems;
 import kr.co.morandi.backend.domain.member.Member;
@@ -21,9 +20,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 class CustomDefenseRecordTest {
-    @DisplayName("커스텀 디펜스 모드 기록이 만들어 졌을 때 시험 날짜는 시작한 시점과 같아야 한다.")
+    @DisplayName("커스텀 디펜스 기록이 만들어 졌을 때 시험 날짜는 시작한 시점과 같아야 한다.")
     @Test
-    public void testDateIsEqualNow() {
+    void testDateIsEqualNow() {
         // given
         CustomDefense customDefense = createCustomDefense();
         List<Problem> problems = getCustomDefenseProblems(customDefense);
@@ -37,9 +36,9 @@ class CustomDefenseRecordTest {
         // then
         assertThat(customDefenseRecord.getTestDate()).isEqualTo(startTime);
     }
-    @DisplayName("커스텀 디펜스 모드 기록이 만들어졌을 때 맞춘 문제 수는 0으로 설정되어야 한다.")
+    @DisplayName("커스텀 디펜스 기록이 만들어졌을 때 맞춘 문제 수는 0으로 설정되어야 한다.")
     @Test
-    public void solvedCountIsZero() {
+    void solvedCountIsZero() {
         // given
         CustomDefense customDefense = createCustomDefense();
         List<Problem> problems = getCustomDefenseProblems(customDefense);
@@ -53,10 +52,9 @@ class CustomDefenseRecordTest {
         // then
         assertThat(customDefenseRecord.getSolvedCount()).isZero();
     }
-
-    @DisplayName("커스텀 디펜스 모드 기록이 만들어졌을 때 총 문제 수 기록은 커스텀 디펜스 문제 수와 같아야 한다.")
+    @DisplayName("커스텀 디펜스 기록이 만들어졌을 때 총 문제 수 기록은 커스텀 디펜스 문제 수와 같아야 한다.")
     @Test
-    public void problemCountIsEqual() {
+    void problemCountIsEqual() {
         // given
         CustomDefense customDefense = createCustomDefense();
         List<Problem> problems = getCustomDefenseProblems(customDefense);
@@ -70,9 +68,9 @@ class CustomDefenseRecordTest {
         // then
         assertThat(customDefenseRecord.getProblemCount()).isEqualTo(customDefense.getProblemCount());
     }
-    @DisplayName("커스텀 디펜스 모드 기록이 만들어졌을 때 초기 전체 소요 시간은 0분 이어야 한다.")
+    @DisplayName("커스텀 디펜스 기록이 만들어졌을 때 초기 전체 소요 시간은 0분 이어야 한다.")
     @Test
-    public void totalSolvedTimeIsZero() {
+    void totalSolvedTimeIsZero() {
         // given
         CustomDefense customDefense = createCustomDefense();
         List<Problem> problems = getCustomDefenseProblems(customDefense);
@@ -86,7 +84,7 @@ class CustomDefenseRecordTest {
         // then
         assertThat(customDefenseRecord.getTotalSolvedTime()).isZero();
     }
-    @DisplayName("커스텀 디펜스 모드 기록이 만들어졌을 때 세부 문제 기록의 정답 여부는 모두 오답이어야 한다.")
+    @DisplayName("커스텀 디펜스 기록이 만들어졌을 때 세부 문제 기록의 정답 여부는 모두 오답이어야 한다.")
     @Test
     void isSolvedFalse() {
         // given
@@ -98,10 +96,9 @@ class CustomDefenseRecordTest {
 
         // when
         CustomDefenseRecord customDefenseRecord = CustomDefenseRecord.create(customDefense, member, startTime, problems);
-        List<ContentProblemRecord> customDefenseProblemRecords = customDefenseRecord.getContentProblemRecords();
 
         // then
-        assertThat(customDefenseProblemRecords)
+        assertThat(customDefenseRecord.getContentProblemRecords())
                 .extracting("isSolved")
                 .containsExactlyInAnyOrder(
                         false,
@@ -109,7 +106,7 @@ class CustomDefenseRecordTest {
                 );
     }
 
-    @DisplayName("커스텀 디펜스 모드 기록이 만들어졌을 때 세부 문제 기록의 문제 기록의 소요 시간은 0분이어야 한다.")
+    @DisplayName("커스텀 디펜스 기록이 만들어졌을 때 세부 문제 기록의 문제 기록의 소요 시간은 0분이어야 한다.")
     @Test
     void solvedTimeIsZero() {
         // given
@@ -120,10 +117,9 @@ class CustomDefenseRecordTest {
 
         // when
         CustomDefenseRecord customDefenseRecord = CustomDefenseRecord.create(customDefense, member, startTime, problems);
-        List<ContentProblemRecord> customDefenseProblemRecords = customDefenseRecord.getContentProblemRecords();
 
         // then
-        assertThat(customDefenseProblemRecords)
+        assertThat(customDefenseRecord.getContentProblemRecords())
                 .extracting("solvedTime")
                 .containsExactlyInAnyOrder(
                         0L,
@@ -131,7 +127,7 @@ class CustomDefenseRecordTest {
                 );
     }
 
-    @DisplayName("커스텀 디펜스 모드 기록이 만들어졌을 때 세부 문제 기록의 제출 횟수는 0회 이어야 한다.")
+    @DisplayName("커스텀 디펜스 기록이 만들어졌을 때 세부 문제 기록의 제출 횟수는 0회 이어야 한다.")
     @Test
     void submitCountIsZero() {
         // given
@@ -142,10 +138,9 @@ class CustomDefenseRecordTest {
 
         // when
         CustomDefenseRecord customDefenseRecord = CustomDefenseRecord.create(customDefense, member, startTime, problems);
-        List<ContentProblemRecord> customDefenseProblemRecords = customDefenseRecord.getContentProblemRecords();
 
         // when & then
-        assertThat(customDefenseProblemRecords)
+        assertThat(customDefenseRecord.getContentProblemRecords())
                 .extracting("submitCount")
                 .containsExactlyInAnyOrder(
                         0L,
@@ -153,7 +148,7 @@ class CustomDefenseRecordTest {
                 );
     }
 
-    @DisplayName("커스텀 디펜스 모드 기록이 만들어졌을 때 세부 문제 기록의 문제 정답 코드는 null 값 이어야 한다.")
+    @DisplayName("커스텀 디펜스 기록이 만들어졌을 때 세부 문제 기록의 문제 정답 코드는 null 값 이어야 한다.")
     @Test
     void solvedCodeIsNull() {
         // given
@@ -164,10 +159,9 @@ class CustomDefenseRecordTest {
 
         // when
         CustomDefenseRecord customDefenseRecord = CustomDefenseRecord.create(customDefense, member, startTime, problems);
-        List<ContentProblemRecord> customDefenseProblemRecords = customDefenseRecord.getContentProblemRecords();
 
         // when & then
-        assertThat(customDefenseProblemRecords)
+        assertThat(customDefenseRecord.getContentProblemRecords())
                 .extracting("solvedCode")
                 .containsExactlyInAnyOrder(
                         null,
