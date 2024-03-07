0098d274e424bdf4f375662f43368990a96418dc
miiiinju1 <gms07073@ynu.ac.kr>
2024-03-07 16:20:12 +0900

    :white_check_mark: DailyTestProblemsTest SpringBootTest 제거

--- src/test/java/kr/co/morandi/backend/domain/contenttype/dailytest/DailyTestProblemsTest.java
+++ src/test/java/kr/co/morandi/backend/domain/contenttype/dailytest/DailyTestProblemsTest.java
@@ -1,13 +1,8 @@
 package kr.co.morandi.backend.domain.contenttype.dailytest;
 
 import kr.co.morandi.backend.domain.problem.Problem;
-import kr.co.morandi.backend.domain.problem.ProblemRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
@@ -15,18 +10,9 @@ import java.util.List;
 
 import static kr.co.morandi.backend.domain.contenttype.tier.ProblemTier.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 @ActiveProfiles("test")
 class DailyTestProblemsTest {
-
-    @Autowired
-    private ProblemRepository problemRepository;
-    @AfterEach
-    void tearDown() {
-        problemRepository.deleteAllInBatch();
-    }
     @DisplayName("오늘의 문제가 만들어졌을 때, 초기의 문제 제출횟수는 0이어야 한다.")
     @Test
     void submitCountIsZero() {
@@ -60,8 +46,6 @@ class DailyTestProblemsTest {
         Problem problem1 = Problem.create(1L, B5, 0L);
         Problem problem2 = Problem.create(2L, S5, 0L);
         Problem problem3 = Problem.create(3L, G5, 0L);
-        List<Problem> problems = List.of(problem1, problem2, problem3);
-        problemRepository.saveAll(problems);
-        return problems;
+        return List.of(problem1, problem2, problem3);
     }
 }
