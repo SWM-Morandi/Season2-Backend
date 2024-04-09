42886fedc9b3d51be2b154844dcbc4aaeb621cc1
miiiinju1 <gms07073@ynu.ac.kr>
2024-04-09 20:33:33 +0900

    :fire: Remove unused import

--- src/main/java/kr/co/morandi/backend/defense_record/domain/model/record/Detail.java
+++ src/main/java/kr/co/morandi/backend/defense_record/domain/model/record/Detail.java
@@ -10,8 +10,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn
