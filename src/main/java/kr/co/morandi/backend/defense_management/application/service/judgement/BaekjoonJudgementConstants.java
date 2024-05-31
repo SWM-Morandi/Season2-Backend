6fa5c3aa74677eef91cf952c157a02351db8ebb0
miiiinju1 <gms07073@ynu.ac.kr>
2024-05-31 17:44:17 +0900

    chore: private 생성자 생성

--- src/main/java/kr/co/morandi/backend/defense_management/application/service/judgement/BaekjoonJudgementConstants.java
+++ src/main/java/kr/co/morandi/backend/defense_management/application/service/judgement/BaekjoonJudgementConstants.java
@@ -1,6 +1,9 @@
-    package kr.co.morandi.backend.defense_management.application.service.judgement;
+package kr.co.morandi.backend.defense_management.application.service.judgement;
 
-    public class BaekjoonJudgementConstants {
-        public static final String BAEKJOON_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36";
-        public static final String BAEKJOON_SUBMIT_URL = "https://www.acmicpc.net/submit/%s";
-    }
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BaekjoonJudgementConstants {
+    public static final String BAEKJOON_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36";
+    public static final String BAEKJOON_SUBMIT_URL = "https://www.acmicpc.net/submit/%s";
+}
