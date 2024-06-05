ff9d1ded758e142c5c14386dc6c132c827ae8190
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-05 20:00:05 +0900

    test: TestConfiguration으로 mocking한 WebClient사용하도록 Test Context 변경

--- src/test/java/kr/co/morandi/backend/IntegrationTestSupport.java
+++ src/test/java/kr/co/morandi/backend/IntegrationTestSupport.java
@@ -1,9 +1,13 @@
 package kr.co.morandi.backend;
 
+import kr.co.morandi.backend.config.WebClientTestConfig;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(WebClientTestConfig.class)
 public abstract class IntegrationTestSupport {
+
 }
