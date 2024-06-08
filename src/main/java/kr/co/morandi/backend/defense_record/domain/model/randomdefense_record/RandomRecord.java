b3ccf74078935df45698cb4886a4dafa007a7360
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-08 19:10:29 +0900

    chore: remove unused import

--- src/main/java/kr/co/morandi/backend/defense_record/domain/model/randomdefense_record/RandomRecord.java
+++ src/main/java/kr/co/morandi/backend/defense_record/domain/model/randomdefense_record/RandomRecord.java
@@ -4,14 +4,13 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import kr.co.morandi.backend.defense_information.domain.model.defense.Defense;
 import kr.co.morandi.backend.defense_information.domain.model.randomdefense.model.RandomDefense;
+import kr.co.morandi.backend.defense_record.domain.model.record.Record;
 import kr.co.morandi.backend.member_management.domain.model.member.Member;
 import kr.co.morandi.backend.problem_information.domain.model.problem.Problem;
-import kr.co.morandi.backend.defense_record.domain.model.record.Record;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 import java.util.Map;
