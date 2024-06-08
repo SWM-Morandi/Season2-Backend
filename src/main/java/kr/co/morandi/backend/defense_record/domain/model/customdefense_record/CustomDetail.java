32d4d8100a6616e8f08aba800c448a4a028f8ea3
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-08 19:10:09 +0900

    chore: remove unused import

--- src/main/java/kr/co/morandi/backend/defense_record/domain/model/customdefense_record/CustomDetail.java
+++ src/main/java/kr/co/morandi/backend/defense_record/domain/model/customdefense_record/CustomDetail.java
@@ -2,16 +2,15 @@ package kr.co.morandi.backend.defense_record.domain.model.customdefense_record;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import kr.co.morandi.backend.defense_record.domain.model.record.Detail;
 import kr.co.morandi.backend.defense_information.domain.model.defense.Defense;
+import kr.co.morandi.backend.defense_record.domain.model.record.Detail;
+import kr.co.morandi.backend.defense_record.domain.model.record.Record;
 import kr.co.morandi.backend.member_management.domain.model.member.Member;
 import kr.co.morandi.backend.problem_information.domain.model.problem.Problem;
-import kr.co.morandi.backend.defense_record.domain.model.record.Record;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
