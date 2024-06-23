087166a812122274ce0e0be36912658d48e4e5e7
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-23 11:34:23 +0900

    chore: recordAdapter 어노테이션 변경 및 파라미터 이름 변경

--- src/main/java/kr/co/morandi/backend/defense_record/infrastructure/adapter/record/RecordAdapter.java
+++ src/main/java/kr/co/morandi/backend/defense_record/infrastructure/adapter/record/RecordAdapter.java
@@ -1,36 +1,36 @@
 package kr.co.morandi.backend.defense_record.infrastructure.adapter.record;
 
+import kr.co.morandi.backend.common.annotation.Adapter;
 import kr.co.morandi.backend.defense_record.application.port.out.record.RecordPort;
 import kr.co.morandi.backend.defense_record.domain.model.record.Detail;
 import kr.co.morandi.backend.defense_record.domain.model.record.Record;
 import kr.co.morandi.backend.defense_record.infrastructure.persistence.record.RecordRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
+@Adapter
 @RequiredArgsConstructor
 public class RecordAdapter implements RecordPort {
 
     private final RecordRepository recordRepository;
     @Override
-    public Optional<Record<? extends Detail>> findRecordById(Long recordId) {
-        return recordRepository.findById(recordId);
+    public Optional<Record<? extends Detail>> findRecordById(Long defenseRecordId) {
+        return recordRepository.findById(defenseRecordId);
     }
 
     @Override
-    public Optional<Record<? extends Detail>> findRecordFetchJoinWithDetail(Long recordId) {
-        return recordRepository.findRecordFetchJoinWithDetail(recordId);
+    public Optional<Record<? extends Detail>> findRecordFetchJoinWithDetail(Long defenseRecordId) {
+        return recordRepository.findRecordFetchJoinWithDetail(defenseRecordId);
     }
 
     @Override
-    public Optional<Record<? extends Detail>> findRecordFetchJoinWithDetailAndProblem(Long recordId) {
-        return recordRepository.findRecordFetchJoinWithDetailAndProblem(recordId);
+    public Optional<Record<? extends Detail>> findRecordFetchJoinWithDetailAndProblem(Long defenseRecordId) {
+        return recordRepository.findRecordFetchJoinWithDetailAndProblem(defenseRecordId);
     }
 
     @Override
-    public void saveRecord(Record<? extends Detail> record) {
-        recordRepository.save(record);
+    public void saveRecord(Record<? extends Detail> defenseRecord) {
+        recordRepository.save(defenseRecord);
     }
 }
